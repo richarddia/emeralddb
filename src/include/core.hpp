1875ee40bf8fd65e1e36bdb91d1e2e94405b90a3
Xin Li <li.xin.ece@gmail.com>
2026-09-02 22:31:49 -0700

    Dev_03_02: ossQueue and ossPrimitiveFileOp

--- src/include/core.hpp
+++ src/include/core.hpp
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <linux/limits.h>
 #include <sys/time.h>
 #include <time.h>
---
+++ src/include/ossQueue.hpp
@@ -0,0 +1,82 @@
+/******************************************************************************
+   Copyright (C) 2013 SequoiaDB Software Inc.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+*******************************************************************************/
+#ifndef OSSQUEUE_HPP__
+#define OSSQUEUE_HPP__
+
+#include<queue>
+#include<boost/thread.hpp>
+#include<boost/thread/thread_time.hpp>
+
+#include<core.hpp>
+
+template<typename Data>
+class ossQueue {
+private:
+    std::queue<Data> _queue;
+    boost::mutex _mutex;
+    boost::condition_variable _cond;
+public:
+    unsigned int size() {
+        boost::mutex::scoped_lock _lock(_mutex);
+        return (unsigned int) _queue.size();
+    }
+
+    void push(const Data& data) {
+        boost::mutex::scoped_lock _lock(_mutex);
+        _queue.push(data);
+        _lock.unlock();
+        _cond.notify_one();
+    }
+
+    bool empty() const {
+        boost::mutex::scoped_lock _lock(_mutex);
+        return _queue.empty();
+    }
+
+    bool try_pop(Data& value) {
+        boost::mutex::scoped_lock _lock(_mutex);
+        if (_queue.empty()) {
+            return false;
+        }
+        value = _queue.front();
+        _queue.pop();
+        return true;
+    }
+
+    void wait_and_pop(Data& value) {
+        boost::mutex::scoped_lock _lock(_mutex);
+        while (_queue.empty()) {
+            _cond.wait(_lock);
+        }
+        value = _queue.front();
+        _queue.pop();
+    }
+
+    void time_wait_and_pop(Data& value, long long millsec) {
+        boost::system_time const timeout = boost::get_system_time() + boost::posix_time::millisec(millsec);
+        boost::mutex::scoped_lock _lock(_mutex);
+        while (_queue.empty()) {
+            // thread will wait as soon as timed_wait is called
+            if (!_cond.timed_wait(&_lock, timeout)) {
+                return false;
+            }
+        }
+        value = _queue.front();
+        _queue.pop();
+        return true;
+    }
+
+};
+
+
+#endif
